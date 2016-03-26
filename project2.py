52559d74fce3ed85c863c6722df672feb4278e50
yl3407 <yitingli685@gmail.com>
2016-03-26 18:07:52 -0400

    simple_nba_database_webpage
    columbia coms4111 database project 1

---
+++ project2.py
@@ -0,0 +1,225 @@
+"""
+Columbia W4111 Intro to databases
+Example webserver
+To run locally
+    python server.py
+Go to http://localhost:8111 in your browser
+A debugger such as "pdb" may be helpful for debugging.
+Read about it online.
+"""
+
+import os
+from sqlalchemy import *
+from sqlalchemy.pool import NullPool
+from flask import Flask, request, render_template, g, redirect, Response
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=tmpl_dir)
+
+
+#
+# The following uses the sqlite3 database test.db -- you can use this for debugging purposes
+# However for the project you will need to connect to your Part 2 database in order to use the
+# data
+#
+# XXX: The URI should be in the format of: 
+#
+#     postgresql://USER:PASSWORD@w4111db.eastus.cloudapp.azure.com/username
+#
+# For example, if you had username ewu2493, password foobar, then the following line would be:
+#
+#     DATABASEURI = "postgresql://ewu2493:foobar@w4111db.eastus.cloudapp.azure.com/ewu2493"
+#
+DATABASEURI = "postgresql://hl2915:FZXVCB@w4111db.eastus.cloudapp.azure.com/hl2915"
+
+
+#
+# This line creates a database engine that knows how to connect to the URI above
+#
+engine = create_engine(DATABASEURI)
+
+
+#
+# START SQLITE SETUP CODE
+#
+# after these statements run, you should see a file test.db in your webserver/ directory
+# this is a sqlite database that you can query like psql typing in the shell command line:
+# 
+#     sqlite3 test.db
+#
+# The following sqlite3 commands may be useful:
+# 
+#     .tables               -- will list the tables in the database
+#     .schema <tablename>   -- print CREATE TABLE statement for table
+# 
+# The setup code should be deleted once you switch to using the Part 2 postgresql database
+#
+#
+# END SQLITE SETUP CODE
+#
+
+
+
+@app.before_request
+def before_request():
+  """
+  This function is run at the beginning of every web request 
+  (every time you enter an address in the web browser).
+  We use it to setup a database connection that can be used throughout the request
+  The variable g is globally accessible
+  """
+  try:
+    g.conn = engine.connect()
+  except:
+    print "uh oh, problem connecting to database"
+    import traceback; traceback.print_exc()
+    g.conn = None
+
+@app.teardown_request
+def teardown_request(exception):
+  """
+  At the end of the web request, this makes sure to close the database connection.
+  If you don't the database could run out of memory!
+  """
+  try:
+    g.conn.close()
+  except Exception as e:
+    pass
+
+@app.route('/')
+def home():
+  return render_template("index.html")
+@app.route('/team')
+def index():
+  """
+  request is a special object that Flask provides to access web request information:
+  request.method:   "GET" or "POST"
+  request.form:     if the browser submitted a form, this contains the data in the form
+  request.args:     dictionary of URL arguments e.g., {a:1, b:2} for http://localhost?a=1&b=2
+  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
+  """
+
+  # DEBUG: this is debugging code to see what request looks like
+  print request.args
+
+
+  #
+  # example of a database query
+  #
+  cursor = g.conn.execute("SELECT *  FROM team")
+  rows = cursor.fetchall()
+  data = [ dict(zip(cursor.keys(), row)) for row in rows ]
+
+  context = dict(data = data)
+
+
+  #
+  # render_template looks in the templates/ folder for files.
+  # for example, the below file reads template/index.html
+  #
+  return render_template("data.html", **context)
+
+#
+# This is an example of a different path.  You can see it at
+# 
+#     localhost:8111/another
+#
+# notice that the functio name is another() rather than index()
+# the functions for each app.route needs to have different names
+@app.route('/player')
+def player():
+
+  print request.args
+
+  cursor = g.conn.execute("SELECT *  FROM player p LEFT OUTER JOIN award a on p.player_id = a.id")
+  rows = cursor.fetchall()
+
+  data = [ dict(zip(cursor.keys(), row)) for row in rows ]
+
+  context = dict(data = data)
+  return render_template("data.html", **context)
+#
+@app.route('/schedule')
+def schedule():
+  print request.args
+  cursor = g.conn.execute("SELECT *  FROM schedule;")
+  rows = cursor.fetchall()
+  data = [ dict(zip(cursor.keys(), row)) for row in rows ]
+
+  context = dict(data = data)
+  return render_template("data.html", **context)
+
+
+@app.route('/standing')
+def standing():
+  print request.args
+  cursor = g.conn.execute("SELECT *  FROM standing")
+  rows = cursor.fetchall()
+  data = [ dict(zip(cursor.keys(), row)) for row in rows ]
+
+  context = dict(data = data)
+  return render_template("data.html", **context)
+
+@app.route('/statistics')
+def statistics():
+  print request.args
+  cursor = g.conn.execute("SELECT *  FROM static_player")
+  rows = cursor.fetchall()
+  data = [ dict(zip(cursor.keys(), row)) for row in rows ]
+
+  context = dict(data = data)
+  return render_template("data.html", **context)
+
+# Example of adding new data to the database
+@app.route('/search', methods=['POST'])
+def add(): 
+  name = request.form['name']
+  cursor = g.conn.execute("SELECT *  FROM player p , award a WHERE p.player_id = a.id AND p.player_name = %s ", str(name))
+  rows = cursor.fetchall()
+  data = [ dict(zip(cursor.keys(), row)) for row in rows ]
+  context = dict(data = data)
+  namelist = []
+  for result in rows:
+    namelist.append(str(result['player_name']))
+
+  print namelist
+  if (name in namelist):
+    return render_template("data.html", **context)
+  else:
+   return searchteam(name)
+@app.route('/search/team') 
+def searchteam(name):
+  cursor = g.conn.execute("SELECT * FROM team WHERE team_name = %s", name)
+  rows = cursor.fetchall()
+  data = [ dict(zip(cursor.keys(), row)) for row in rows ]
+  context = dict(data = data)
+  return render_template("data.html", **context)
+
+
+
+
+
+
+if __name__ == "__main__":
+  import click
+
+  @click.command()
+  @click.option('--debug', is_flag=True)
+  @click.option('--threaded', is_flag=True)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8111, type=int)
+  def run(debug, threaded, host, port):
+    """
+    This function handles command line parameters.
+    Run the server using
+        python server.py
+    Show the help text using
+        python server.py --help
+    """
+
+    HOST, PORT = host, port
+    print "running on %s:%d" % (HOST, PORT)
+    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+
+  run()
